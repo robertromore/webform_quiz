64c8939a57700bd03213bb478b6309a2699d30b6
Brian McVeigh <bcmcveigh@gmail.com>
2018-12-28 11:30:45 -0500

    Updated API documentation.

--- webform_quiz.api.php
+++ webform_quiz.api.php
@@ -1,7 +1,15 @@
 <?php
 
+use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Form\FormStateInterface;
 
-function hook_webform_quiz_correct_answer_shown($ajax_response, $form, FormStateInterface $form_state) {
+/**
+ * Perform ajax actions for when a user clicks a quiz response.
+ *
+ * @param \Drupal\Core\Ajax\AjaxResponse $ajax_response
+ * @param $form
+ * @param \Drupal\Core\Form\FormStateInterface $form_state
+ */
+function hook_webform_quiz_correct_answer_shown(AjaxResponse $ajax_response, $form, FormStateInterface $form_state) {
 
 }
